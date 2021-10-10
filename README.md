98a797b102df8f241b13b5dd6a3559c65fc05841
elvinpeng <elvinpeng@tencent.com>
2021-10-10 10:20:40 +0800

    feat: 111. 二叉树的最小深度

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 1. [Easy 108. 将有序数组转换为二叉搜索树](https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/)
 2. [Easy 110. 平衡二叉树](https://leetcode-cn.com/problems/balanced-binary-tree/)
+3. [Easy 111. 二叉树的最小深度](https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/)
 
 ## 2021.10.09
 
---
+++ src/111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,32 @@
+// https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+  if (!root) {
+    return 0
+  }
+
+  const leftMinDepth = minDepth(root.left)
+  const rightMinDepth = minDepth(root.right)
+
+  if (leftMinDepth === 0) {
+    return rightMinDepth + 1
+  }
+
+  if (rightMinDepth === 0) {
+    return leftMinDepth + 1
+  }
+
+  return Math.min(leftMinDepth, rightMinDepth) + 1
+}
