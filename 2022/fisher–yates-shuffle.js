af367f786362cc0c0d28eeb0cfc68693e0e6cfa1
elvinpeng <elvinpeng@tencent.com>
2022-06-13 17:18:51 +0800

    docs: 添加文档

--- 2022/fisher–yates-shuffle.js
+++ 2022/fisher–yates-shuffle.js
@@ -10,6 +10,12 @@ const swap = (numList, i, j) => {
   numList[j] = temp;
 }
 
+/**
+ * Fisher–Yates shuffle 算法随机
+ * 其实和每次随机从原始数组选一个，然后加入到新数组的思路是一模一样的
+ * @param {number[]} numList 原始数组
+ * @returns 随机后的数组
+ */
 const shuffle = (numList = []) => {
   for (let i = 0; i < numList.length; i++) {
     const randomIndex = getRand(i, numList.length - 1);
