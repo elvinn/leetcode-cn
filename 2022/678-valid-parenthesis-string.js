6b4bdb01e6d2f22b57b08ccd79a3a7abc3a96424
elvinpeng <elvinpeng@tencent.com>
2022-06-20 10:06:15 +0800

    feat: 优化 678. 有效的括号字符串代码

--- 2022/678-valid-parenthesis-string.js
+++ 2022/678-valid-parenthesis-string.js
@@ -1,62 +1,48 @@
 // https://leetcode-cn.com/problems/valid-parenthesis-string
 
 /**
- * 用单栈导致复杂度有些高了，双栈存储元素下标的话逻辑会简化不少
+ * 单栈/双栈均可完成，用双栈存储元素下标的话逻辑会简化不少
  * @param {string} s
  * @return {boolean}
  */
 const checkValidString = (s) => {
-  const charStack = []
+  const leftCharStack = []
+  const startStack = []
 
-  for (const char of s) {
-    if (['(', '*'].includes(char)) {
-      charStack.push(char)
-      continue
-    }
-
-    let isMatched = false
-    let tempStarCount = 0 // 临时取出的 '*' 数量
-    while (charStack.length > 0) {
-      const popChar = charStack.pop()
-      if (popChar === '(') {
-        isMatched = true
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i]
+    switch (char) {
+      case '(':
+        leftCharStack.push(i)
         break
-      }
-
-      if (popChar === '*') {
-        tempStarCount += 1
-        continue
-      }
-
-      // popChar 为 '('
-      if (!tempStarCount) {
-        return false
-      }
-
-      charStack.push(popChar)
-      break
-    }
-    if (!isMatched) {
-      if (!tempStarCount) {
-        return false
-      }
-
-      tempStarCount -= 1
-    }
-
-    charStack.push(...Array.from({ length: tempStarCount }).fill('*'))
-  }
-
-  let leftOneCount = 0
-  for (const char of charStack) {
-    if (char === '(') {
-      leftOneCount += 1
-    }
-
-    if (char === '*') {
-      leftOneCount = Math.max(0, leftOneCount - 1)
+      case '*':
+        startStack.push(i)
+        break
+      case ')':
+        if (leftCharStack.length > 0) {
+          leftCharStack.pop()
+        } else if (startStack.length > 0) {
+          startStack.pop()
+        } else {
+          return false
+        }
+        break
+      default:
+        break
     }
   }
 
-  return leftOneCount === 0
+  while (leftCharStack.length > 0) {
+    if (!startStack.length) {
+      return false
+    }
+
+    const leftCharIndex = leftCharStack.pop()
+    const starCharIndex = startStack.pop()
+    if (leftCharIndex > starCharIndex) {
+      return false
+    }
+  }
+
+  return true
 }
