deb8bb406b4bb42718383fb4c9db55dd8b957b97
elvinpeng <elvinpeng@tencent.com>
2024-07-28 11:10:02 +0800

    feat: 225 invert-binary-tree

---
+++ 2024/226-invert-binary-tree.js
@@ -0,0 +1,26 @@
+// https://leetcode.cn/problems/invert-binary-tree/?envType=study-plan-v2&envId=top-100-liked
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (!root) {
+    return root;
+  }
+
+  const temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+};
